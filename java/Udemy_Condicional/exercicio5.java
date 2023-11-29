e289cc15c3d973b3a2850e6fc9fa9fc5fa18dd39
Luiz Grenteski <luiz.grenteski@rrs-software.net>
2023-11-29 13:27:25 -0300

    alteração switch exercicio 5

--- java/Udemy_Condicional/exercicio5.java
+++ java/Udemy_Condicional/exercicio5.java
@@ -17,59 +17,55 @@ public class exercicio5 {
      */
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.printf("Código |   Especificação  | Preço  %n");
         System.out.printf("   1   |  Cachorro Quente | R$4,00 %n");
         System.out.printf("   2   |      X-Salada    | R$4,50 %n");
         System.out.printf("   3   |      X-Bacon     | R$5,00 %n");
         System.out.printf("   4   |  Torrada Simples | R$2,00 %n");
         System.out.printf("   5   |   Refrigerante   | R$1,50 %n");
-        
+
         int opcao = 0;
         double valorTotal = 0.0;
-        
+
         System.out.printf("Escolha os produtos pelo código (Digite 0 para terminar)%n");
-        
+
         do {
             System.out.printf("(0 para sair) Produto: ");
-        opcao = sc.nextInt();
-        
-        switch(opcao) {
-            case 0:
-                System.out.println();
-                break;
-                
-            case 1:
-                valorTotal += 4.00;
-                System.out.printf("Produto selecionado: Cachorro Quente | Valor Parcial R$ %.2f%n", valorTotal);
-                break;
-            
-            case 2:
-                valorTotal += 4.50;
-                System.out.printf("Produto selecionado: X-Salada | Valor Parcial R$ %.2f%n", valorTotal);
-                break;
-            
-            case 3:
-                valorTotal += 5.00;
-                System.out.printf("Produto selecionado: X-Bacon | Valor Parcial R$ %.2f%n", valorTotal);
-                break;
-            
-            case 4:
-                valorTotal += 2.00;
-                System.out.printf("Produto selecionado: Torrada Simples | Valor Parcial R$ %.2f%n", valorTotal);
-                break;
-            
-            case 5:
-                valorTotal += 1.50;
-                System.out.printf("Produto selecionado: Refrigerante | Valor Parcial R$ %.2f%n", valorTotal);
-                break;
-            
-            default:
-                System.out.printf("Opção inválida! %n");
-                break;
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 0 -> System.out.println();
+
+                case 1 -> {
+                    valorTotal += 4.00;
+                    System.out.printf("Produto selecionado: Cachorro Quente | Valor Parcial R$ %.2f%n", valorTotal);
+                }
+
+                case 2 -> {
+                    valorTotal += 4.50;
+                    System.out.printf("Produto selecionado: X-Salada | Valor Parcial R$ %.2f%n", valorTotal);
+                }
+
+                case 3 -> {
+                    valorTotal += 5.00;
+                    System.out.printf("Produto selecionado: X-Bacon | Valor Parcial R$ %.2f%n", valorTotal);
+                }
+
+                case 4 -> {
+                    valorTotal += 2.00;
+                    System.out.printf("Produto selecionado: Torrada Simples | Valor Parcial R$ %.2f%n", valorTotal);
+                }
+
+                case 5 -> {
+                    valorTotal += 1.50;
+                    System.out.printf("Produto selecionado: Refrigerante | Valor Parcial R$ %.2f%n", valorTotal);
+                }
+
+                default -> System.out.printf("Opção inválida! %n");
             }
-        } while(opcao != 0);
-        
+        } while (opcao != 0);
+
         System.out.printf("Valor Total: R$ %.2f %n", valorTotal);
         sc.close();
     }
