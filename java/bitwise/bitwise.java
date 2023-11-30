c0a1128272c59fb2b14587c3980a1f4419c4f261
Luiz Grenteski <luiz.grenteski@rrs-software.net>
2023-11-30 10:51:33 -0300

    BitWise

---
+++ java/bitwise/bitwise.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package bitwise;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class bitwise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int mask = 0b100000;
+        int n = sc.nextInt();
+        
+        if((n & mask) !=0) {
+            System.out.println("6th bit is true!");
+        } else {
+            System.out.println("6th bit is true!");
+        }
+        sc.close();
+    }
+}
