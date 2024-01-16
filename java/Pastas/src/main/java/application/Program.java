ea6f208876b355919c5ddc2a5345f3cc6844a385
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-16 10:59:50 -0300

    Folders

---
+++ java/Pastas/src/main/java/application/Program.java
@@ -0,0 +1,43 @@
+
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+        
+        File path = new File(strPath);
+        
+        File[] folders = path.listFiles(File::isDirectory);
+        
+        System.out.println("Folders: ");
+        for(File folder : folders) {
+            System.out.println(folder);
+        }
+        
+        System.out.println();
+        File[] files = path.listFiles(File::isFile);
+        
+        System.out.println("Files: ");
+        for(File file : files) {
+            System.out.println(file);
+        }
+        
+        boolean sucess = new File(strPath + "\\subdir").mkdir();
+        
+        System.out.println("Directory created sucessfully: " + sucess);
+
+        sc.close();
+    }
+}
