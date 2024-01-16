3aed743b6fc2afbb45de5f8a8d133cc3e40e0b3c
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-16 10:49:06 -0300

    FileWriter

---
+++ java/FileWriter/src/main/java/application/Program.java
@@ -0,0 +1,29 @@
+
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+        
+        String path = "c:\\temp\\out.txt";
+        
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for(String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException ex) {
+            ex.getStackTrace();
+        }
+    }
+}
