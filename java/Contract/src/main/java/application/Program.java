3ca8d97feb56150279f8c743024f5eae7d73225a
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-18 10:54:41 -0300

    Exercício com Interface

---
+++ java/Contract/src/main/java/application/Program.java
@@ -0,0 +1,51 @@
+
+package application;
+
+import entities.Contract;
+import entities.Installment;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+import services.ContractService;
+import services.PaypalService;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+        System.out.println("Entre os dados do contrato:");
+        System.out.print("Número: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+        
+        Contract contract = new Contract(number, date, totalValue);
+        
+        System.out.print("Digite o número de parcelas: ");
+        int  n = sc.nextInt();
+        
+        ContractService contractService = new ContractService(new PaypalService());
+        
+        contractService.processContract(contract, n);
+        
+        System.out.println("Parcelas:");
+        for(Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
+        
+        sc.close();
+    }
+}
