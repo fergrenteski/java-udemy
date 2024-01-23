e18ae97a228779fb5630278baa98899862588443
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-23 08:43:12 -0300

    ConsumerExample

---
+++ java/ConsumerExample/src/main/java/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+import util.priceUpdate;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        
+        list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
+        // priceUpdate()
+        // Product::staticPriceUpdate
+        // Product::nonStaticPriceUpdate
+        // Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+        
+        list.forEach(System.out::println);
+    }
+}
