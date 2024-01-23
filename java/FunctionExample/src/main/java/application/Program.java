a0093d4e935a6c98386317aa7ee3c6c2e90ce5e2
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2024-01-23 09:00:49 -0300

    FunctionExample

---
+++ java/FunctionExample/src/main/java/application/Program.java
@@ -0,0 +1,35 @@
+
+package application;
+
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import util.UperCaseName;
+
+/**
+ *
+ * @author luiz.grenteski
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        // new UperCaseName()
+        // Product::staticUpperCaseName
+        // Product::nonStaticUpperCaseName
+        // Function<Product, String> func = p -> p.getName().toUpperCase()
+        
+        names.forEach(System.out::println);
+    }
+}
